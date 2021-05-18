76482aa59e593808892d942225548965bccf504c
Marouf Wani <71086766+maroufwani@users.noreply.github.com>
2021-05-18 11:26:45 +0530

    Create theme-switcher.js

---
+++ theme-switcher/theme-switcher.js
@@ -0,0 +1,18 @@
+<link rel="stylesheet" type="text/css" href="https://cdn.vptofficial.me/light-lite/styles.css" title="lightlite" />
+<link rel="stylesheet" type="text/css" href="https://cdn.vptofficial.me/gradient-blue-lite/styles.css" title="gradientbluelite" />
+<link rel="stylesheet" type="text/css" href="https://cdn.vptofficial.me/dark-lite/styles.css" title="draklight" />
+<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/VPTOfficial/VistaPanel-Customizations/theme-switcher/switcher.js"></script>
+<button data-toggle="dropdown" class="btn dropdown-toggle user-preferences-btn">
+<span>Theme Options</span>
+</button>
+<ul class="dropdown-menu dropdown-menu-right">
+<li>
+<a href="#" onclick="setActiveStyleSheet('lightlite'); return false;">Light Lite</a>
+</li>
+<li>
+<a href="#" onclick="setActiveStyleSheet('gradientbluelite'); return false;">Gradient Blue Lite</a>
+</li>
+<li>
+<a href="#" onclick="setActiveStyleSheet('draklight'); return false;">Dark Light</a>
+</li>
+</ul>
