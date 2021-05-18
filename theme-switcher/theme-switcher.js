67133d02d4098fcba1d7a4c7c7f19d50eb4f9ea4
Marouf Wani <71086766+maroufwani@users.noreply.github.com>
2021-05-18 12:49:17 +0530

    Update theme-switcher.js

--- theme-switcher/theme-switcher.js
+++ theme-switcher/theme-switcher.js
@@ -1,18 +1,16 @@
-<link rel="stylesheet" type="text/css" href="https://cdn.vptofficial.me/light-lite/styles.css" title="lightlite" />
-<link rel="stylesheet" type="text/css" href="https://cdn.vptofficial.me/gradient-blue-lite/styles.css" title="gradientbluelite" />
-<link rel="stylesheet" type="text/css" href="https://cdn.vptofficial.me/dark-lite/styles.css" title="draklight" />
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/WybeNetwork/VistaPanel-Themes@master/Bootswatch_Flatly/icon_spritemap.css" title="bootswatchflaty">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/WybeNetwork/VistaPanel-Themes@master/Bootswatch_Flatly/styles.css" title="bootswatchflaty">
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/WybeNetwork/VistaPanel-Themes@master/Icy/icon_spritemap.css" title="icy">
+<link rel="stylesheet" type="text/css" https://cdn.jsdelivr.net/gh/WybeNetwork/VistaPanel-Themes@master/Icy/styles.css" title="icy">
 <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/VPTOfficial/VistaPanel-Customizations/theme-switcher/switcher.js"></script>
 <button data-toggle="dropdown" class="btn dropdown-toggle user-preferences-btn">
 <span>Theme Options</span>
 </button>
 <ul class="dropdown-menu dropdown-menu-right">
 <li>
-<a href="#" onclick="setActiveStyleSheet('lightlite'); return false;">Light Lite</a>
+<a href="#" onclick="setActiveStyleSheet('bootswatchflaty'); return false;">Boots Watch</a>
 </li>
 <li>
-<a href="#" onclick="setActiveStyleSheet('gradientbluelite'); return false;">Gradient Blue Lite</a>
-</li>
-<li>
-<a href="#" onclick="setActiveStyleSheet('draklight'); return false;">Dark Light</a>
+<a href="#" onclick="setActiveStyleSheet('ict'); return false;">Icy</a>
 </li>
 </ul>
